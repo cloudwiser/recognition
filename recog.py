8e8d41eddb4edbec92376364ba5bc8a18d184cd9
Nick <nick@cloudwise.co>
2019-02-17 17:23:53 +0000

    Update recog.py

--- recog.py
+++ recog.py
@@ -329,21 +329,20 @@ def get_YOLO3_output_layers(net):
 
 # Get the SSD MobileNet candidate object boxes
 def get_SSD_objects(region, net, net_params):
-    scale = 0.007843
-    region_resized = cv2.resize(region,(300,300)) # resize frame for prediction
+    # resize frame for prediction
+    # region_resized = cv.resize(region, (300,300))
     # Create a 4D blob from the region
-    # blob = cv.dnn.blobFromImage(region, swapRB=True, crop=False)
-    blob = cv.dnn.blobFromImage(region_resized, scale, size=(300, 300), (127.5, 127.5, 127.5), False)    
+    blob = cv.dnn.blobFromImage(region, swapRB=True, crop=False)
+    # blob = cv.dnn.blobFromImage(region_resized, 0.007843, (300, 300), (127.5, 127.5, 127.5), False)    
     net.setInput(blob)
     # Run the forward pass to get object boxes from the output layers
     return net.forward(net_params)
 
 # Get the YOLO v3 candidate object boxes
 def get_YOLO3_objects(region, net, net_params):
-    scale = 0.00392
     # Create a 4D blob from the region
     # blob = cv.dnn.blobFromImage(region, scale, size=(416, 416), mean=(0,0,0), swapRB=False, crop=False)
-    blob = cv.dnn.blobFromImage(region, scale, size=(544, 544), mean=(0,0,0), swapRB=True, crop=False)
+    blob = cv.dnn.blobFromImage(region, 0.00392, (544, 544), (0,0,0), swapRB=True, crop=False)
     net.setInput(blob)
     # Run the forward pass to get object boxes from the output layers
     return net.forward(net_params)
@@ -351,7 +350,7 @@ def get_YOLO3_objects(region, net, net_params):
 # Get the Faster RCNN ResNet50 candidate object boxes
 def get_Faster_RCNN_objects(region, net, net_params):
     # Create a 4D blob from the region
-    blob = cv.dnn.blobFromImage(region, size=(300, 300), swapRB=True, crop=False)
+    blob = cv.dnn.blobFromImage(region, (300, 300), swapRB=True, crop=False)
     # blob = cv.dnn.blobFromImage(region, size=(300, 300), mean=(103.939, 116.779, 123.68), swapRB=False, crop=False)    
     net.setInput(blob)
     # Run the forward pass to get object boxes from the output layers
